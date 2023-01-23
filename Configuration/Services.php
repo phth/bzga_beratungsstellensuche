02873d50917c0d0350cd60e3e02397a9ee6af1c3
lina.wolf <lwolf@w-commerce.de>
2023-01-23 11:20:31 +0000

    [TASK] Introduce DI

--- Configuration/Services.php
+++ Configuration/Services.php
@@ -9,13 +9,23 @@ declare(strict_types=1);
  * LICENSE.txt file that was distributed with this source code.
  */
 
+use Bzga\BzgaBeratungsstellensuche\Service\Geolocation\Decorator\GeolocationServiceCacheDecorator;
+use Bzga\BzgaBeratungsstellensuche\Service\Geolocation\GeolocationService;
+use Bzga\BzgaBeratungsstellensuche\Service\Geolocation\GeolocationServiceInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void {
     $services = $containerConfigurator->services();
     $services->defaults()
-        ->public()
+        ->private()
         ->autowire()
         ->autoconfigure();
+    $services->load('Bzga\\BzgaBeratungsstellensuche\\', __DIR__ . '/../Classes/')->exclude([
+        __DIR__ . '/../Classes/Domain/Model',
+    ]);
+
+    $services->alias(GeolocationServiceInterface::class, GeolocationService::class);
+
+    $services->set(GeolocationServiceCacheDecorator::class)->public();
 };
