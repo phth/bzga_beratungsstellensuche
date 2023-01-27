6463953af36cf8340514893a2db252adb348d373
lina.wolf <lwolf@w-commerce.de>
2023-01-27 06:59:33 +0000

    [TASK] Remove check for TYPO3_MODE ext_tables is only loaded in BE anyway, the constant deprecated

--- ext_tables.php
+++ ext_tables.php
@@ -5,8 +5,6 @@ if (!defined('TYPO3')) {
 }
 
 // We check if either curl is installed or allow_url_fopen
-if (TYPO3_MODE === 'BE') {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['bzgaberatungsstellensuche'] = [
-        StatusAllowUrlFopenOrCurlReport::class,
-    ];
-}
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['bzgaberatungsstellensuche'] = [
+    StatusAllowUrlFopenOrCurlReport::class,
+];
