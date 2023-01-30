84afa625fa2e00181bf691f97c381a9aedde14c5
Sebastian Schreiber <me@schreibersebastian.de>
2023-01-30 14:16:33 +0100

    TASK: Refactor custom factories
    Resolves: #52

--- Classes/Service/Geolocation/GeolocationService.php
+++ Classes/Service/Geolocation/GeolocationService.php
@@ -12,15 +12,62 @@ declare(strict_types=1);
 namespace Bzga\BzgaBeratungsstellensuche\Service\Geolocation;
 
 use Bzga\BzgaBeratungsstellensuche\Domain\Model\Dto\Demand;
+use Bzga\BzgaBeratungsstellensuche\Domain\Model\GeoPositionDemandInterface;
+use Bzga\BzgaBeratungsstellensuche\Domain\Model\GeopositionInterface;
+use Bzga\BzgaBeratungsstellensuche\Service\SettingsService;
 use Geocoder\Exception\CollectionIsEmpty;
 use Geocoder\Location;
+use Geocoder\Provider\Provider;
 use Geocoder\Query\GeocodeQuery;
 
 /**
  * @author Sebastian Schreiber
  */
-class GeolocationService extends AbstractGeolocationService
+class GeolocationService implements GeolocationServiceInterface
 {
+    /**
+     * @var string
+     */
+    public const DISTANCE_SQL_FIELD = '(6371.01 * acos(cos(radians(%1$f)) * cos(radians(latitude)) * cos(radians(longitude) - radians(%2$f)) + sin(radians(%1$f) ) * sin(radians(latitude))))';
+
+    /**
+     * @var float
+     */
+    public const EARTH_RADIUS = 6371.01;
+
+    /**
+     * @var int
+     */
+    public const DEFAULT_RADIUS = 10;
+
+    protected SettingsService $settingsService;
+
+    protected Provider $geocoder;
+
+    public function __construct(SettingsService $settingsService, Provider $geocoder)
+    {
+        $this->settingsService = $settingsService;
+        $this->geocoder = $geocoder;
+    }
+
+    public function calculateDistance(GeopositionInterface $demandPosition, GeopositionInterface $locationPosition): float
+    {
+        return self::EARTH_RADIUS * acos(
+            cos(deg2rad($demandPosition->getLatitude())) * cos(deg2rad($locationPosition->getLatitude())) * cos(
+                deg2rad($locationPosition->getLongitude()) - deg2rad($demandPosition->getLongitude())
+            ) + sin(deg2rad($demandPosition->getLatitude())) * sin(deg2rad($locationPosition->getLatitude()))
+        );
+    }
+
+    public function getDistanceSqlField(GeoPositionDemandInterface $demandPosition, string $table, string $alias = 'distance'): string
+    {
+        return sprintf(
+            self::DISTANCE_SQL_FIELD,
+            $demandPosition->getLatitude(),
+            $demandPosition->getLongitude(),
+            $demandPosition->getKilometers()
+        ) . ' AS ' . $alias;
+    }
     public function findAddressByDemand(Demand $demand): ?Location
     {
         if ($demand->getLocation()) {
